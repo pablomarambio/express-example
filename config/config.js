b117c5968f446926fa235435b973c7279b04779c
Pablo Marambio <yo@pablomarambio.cl>
2018-02-05 14:31:31 -0300

    Database url for heroku

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     database: 'customerdb2_test'
   },
   production: {
-    use_env_variable: true,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres'
   }
 };
