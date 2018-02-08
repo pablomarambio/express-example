a685a8f7ce69616092ddfca561f2546fbfb29722
Pablo Marambio <yo@pablomarambio.cl>
2018-02-08 19:01:26 -0300

    Tests DRY

--- test/integration/tenant-creation.test.js
+++ test/integration/tenant-creation.test.js
@@ -5,105 +5,117 @@ var Bluebird = require('bluebird');
 var expect   = require('expect.js');
 var request  = require('supertest');
 
-describe('app', function () {
+describe('global', () => {
+
   before(function () {
       return require('../../models').sequelize.sync();
   });
   
-  beforeEach(function () {
+  beforeEach(function (done) {
     this.models = require('../../models');
-
-    return Bluebird.all([
-      //this.models.Task.destroy({ truncate: true, cascade: true }),
-      this.models.Tenant.destroy({ truncate: true, cascade: true })
-    ]);
+    this.models.Tenant.create({ email: 'tenant@tenant.com' }).then((newTenant) => {
+      this.tenant = newTenant;
+      done();
+    });
   });
-  describe('web frontend', function () {
 
-    describe('/setup', function () {
+  afterEach(function(done) {
+    this.models.Tenant.destroy({ truncate: true, cascade: true });
+    done();
+  });
 
-      it('loads correctly', function (done) {
-        request(app).get('/setup').expect(200, done);
-      });
+  describe('app', function () {
 
-      it('lists a tenant if there is one', function (done) {
-        this.models.Tenant.create({ email: 'tenant@tenant.com' }).then(function () {
+    describe('web frontend', function () {
+
+      describe('/setup', function () {
+
+        it('loads correctly', function (done) {
+          request(app).get('/setup').expect(200, done);
+        });
+
+        it('lists a tenant if there is one', function (done) {
           request(app).get('/setup').expect(/tenant@tenant.com/, done);
-        })
-      });
-
-    });
-
-    describe('/tenant-view', function () {
-
-      it('loads correctly', function (done) {
-        this.models.Tenant.create({ email: 'tenant@tenant.com' }).bind(this)
-        .then(function (tenant) {
-          request(app).get('/tenant-view/' + tenant.id).expect(200, done);
         });
+
       });
 
-      it('lists the customers for the tenant', function (done) {
-        var tenantId;
-        this.models.Tenant.create({ email: 'tenant@tenant.com' }).bind(this)
-        .then(function (tenant) {
-          tenantId = tenant.id;
-          this.models.Customer.create({ nombre_completo: 'juan', rut: 8457638, TenantId: tenantId });
-        })
-        .then(function () {
-          request(app).get('/tenant-view/' + tenantId).expect(/juan/, done);
+      describe('/tenant-view', function () {
+
+        it('loads correctly', function (done) {
+          request(app).get('/tenant-view/' + this.tenant.id).expect(200, done);
         });
-      });
 
-    });
-  });
+        it('lists the customers for the tenant', function (done) {
+          var tenantId = this.tenant.id;
+          this.models.Customer
+            .create({ nombre_completo: 'juan', rut: 8457638, TenantId: tenantId })
+            .then(function () {
+              request(app).get('/tenant-view/' + tenantId).expect(/juan/, done);
+            });
+        });
 
-  describe("api", function () {
-
-    it('lists the customers for the tenant', function (done) {
-      var tenantId;
-      this.models.Tenant.create({ username: 'tenant@tenant.com' }).bind(this)
-      .then(function (tenant) {
-        tenantId = tenant.id;
-        this.models.Customer.create({ nombre_completo: 'bea', rut: 7956772, TenantId: tenantId });
-      }).then(function () {
-        request(app).get('/tenants/' + tenantId + "/customers").expect(/bea/, done);
       });
     });
 
-    it('returns 404 when trying to list the customers from an unknown tenant', function (done) {
-      var tenantId = 234234234;
-      request(app).get('/tenants/' + tenantId + "/customers").expect(404, done);
-    });
+    describe("api", function () {
 
+      describe("customers", () => {
 
-    it('creates a customer', function (done) {
-      var tenantId;
-      this.models.Tenant.create({ email: 'tenant@tenant.com' }).bind(this)
-      .then(function (tenant) {
-        tenantId = tenant.id;
-        request(app)
-          .post('/tenants/' + tenantId + "/customers")
-          .send({nombre_completo: "Pepito", rut: 15776844})
-          .expect(302, done)
-      });
-    });
+        describe("list", () => {
 
-    it('forbids a duplicated customer rut for the same tenant', function (done) {
-      var tenantId;
-      this.models.Tenant.create({ email: 'tenant@tenant.com' }).bind(this)
-      .then(function (tenant) {
-        tenantId = tenant.id;
-        request(app)
-          .post('/tenants/' + tenantId + "/customers")
-          .send({nombre_completo: "Pepito", rut: 14569484})
-          .then(function() {
+          it('lists the customers for the tenant', function (done) {
+            var tenantId = this.tenant.id;
+              this.models.Customer
+              .create({ nombre_completo: 'bea', rut: 7956772, TenantId: tenantId })
+              .then(function () {
+                request(app).get('/tenants/' + tenantId + "/customers").expect(/bea/, done);
+              });
+          });
+
+          it('returns 404 when trying to list the customers from an unknown tenant', function (done) {
+            var tenantId = 234234234;
+            request(app).get('/tenants/' + tenantId + "/customers").expect(404, done);
+          });
+
+        });
+
+        describe("create", () => {
+
+          it("works with x-www-urlencoded", function(done) {
+            var tenantId;
+            tenantId = this.tenant.id;
             request(app)
               .post('/tenants/' + tenantId + "/customers")
-              .send({nombre_completo: "Pepito 2", rut: 14569484})
-              .expect(403, done)
+              .send({nombre_completo: "Pepito", rut: 15776844})
+              .expect(302, done);
           });
+
+          it('works with JSON', function (done) {
+            var tenantId = this.tenant.id;
+            request(app)
+              .post('/tenants/' + tenantId + "/customers")
+              .send({nombre_completo: "Pepito", rut: 15776844})
+              .expect(302, done);
+          });
+
+          it('forbids a duplicated customer rut for the same tenant', function (done) {
+            var tenantId = this.tenant.id;
+            request(app)
+              .post('/tenants/' + tenantId + "/customers")
+              .send({nombre_completo: "Pepito", rut: 14569484})
+              .then(function() {
+                request(app)
+                  .post('/tenants/' + tenantId + "/customers")
+                  .send({nombre_completo: "Pepito 2", rut: 14569484})
+                  .expect(403, done);
+              });
+          });
+          
+        });
+        
       });
     });
   });
+
 });
